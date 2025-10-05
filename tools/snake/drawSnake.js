606cc46898a8b47e497e468716caa4832a0b89b7
bxzing <62268923+r00kieL@users.noreply.github.com>
2025-10-06 01:59:13 +0800

    添加了区分蛇头蛇身的颜色

--- tools/snake/drawSnake.js
+++ tools/snake/drawSnake.js
@@ -4,10 +4,11 @@ import { gridToPixel } from "../gridToPixel.js";
 
 function drawSnake() {
     const ctx = config.gameCanvas.getContext("2d");
-    ctx.fillStyle = "black";
+    const head = snakeConfig.body[0];
 
     for (let block of snakeConfig.body) {
-        const pixel = gridToPixel(block)
+        const pixel = gridToPixel(block);
+        block === head ? ctx.fillStyle = "#000000" : ctx.fillStyle = "#444444";
         ctx.fillRect(pixel.x, pixel.y, 20, 20);
     }
 }
